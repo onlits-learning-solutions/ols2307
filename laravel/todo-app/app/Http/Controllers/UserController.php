e38226717a8671ea5bf26c8b54db8b72c59fb916
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2024-07-13 21:09:07 +0530

    20240713

--- laravel/todo-app/app/Http/Controllers/UserController.php
+++ laravel/todo-app/app/Http/Controllers/UserController.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    function authenticate()
+    function authenticate(Request $request)
     {
-        return view('home');
+        return $request;
     }
 }
